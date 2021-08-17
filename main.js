2fcf4c4145c9e7344ce66129607d8179e86361ef
Vaibhav Kumar <60957669+vkvbit@users.noreply.github.com>
2021-08-17 20:27:51 +0530

    added home

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
+var homeTabLink = "https://vaibhavkumar.ml";
 var firstLink = "./classic/index.html";
 var secondLink = "./notebook_web/index.html";
 
+var homeTabAddress = "secure | https://vaibhavkumar.ml";
 var firstAddress = "secure | https://about.vaibhavkumar.ml/classic/index.html";
 var secondAddress = "secure | https://about.vaibhavkumar.ml/notebook_web/index.html";
+
+function homeTab() {
+	document.getElementById("iFrame").src = homeTabLink;
+	document.getElementById("addressBar").innerHTML = homeTabAddress;
+}
 
 function tabLinkFirst() {
 	document.getElementById("iFrame").src = firstLink;
